8b68878062bdcc7881541176d082af7c366f5be6
Claude Ishimwe <dpqb12haikuo@gmail.com>
2023-07-24 13:09:28 +0200

    Last commit

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,50 @@
-console.log("Exercise 1 - Word Highlighter");
+// console.log("Exercise 1 - Word Highlighter");
+// const paragraph = document.querySelector("#myParagraph");
+// const str = document.querySelector("#myParagraph").innerHTML.split(" ");
+// const matches = [];
+
+// str.forEach((element) => {
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = 0; j < str[i].length; j++) {
+//       if ((element[j] = str[i][j])) {
+//       }
+//     }
+//   }
+// });
+// // matches.forEach(function (item) {
+// //   if (matches.indexOf(item) < 0) {
+// //     matches.push(`<b>${item}</b>`);
+// //   }
+// /
+
+// const paragraph = document.querySelector("#myParagraph");
+// const occurances = {};
+// const str = paragraph.innerHTML.split(" ");
+// var currentElement = str[0];
+// str.forEach((word) =>{
+//     for(let i = 0; i < str.length; i++){
+//         for(let j = 0; j < str[i].length; j++){
+//             if(word == str[i][j]){
+//                 occurance
+//             }
+//         }
+//     }
+// })
+
+// const paragraph = document.querySelector("#myParagraph");
+// // const occurances = {};
+// const str = paragraph.innerHTML;
+// var wordCounts = {};
+// var words = str.split(" ");
+
+// for (var i = 0; i < words.length; i++) {
+//   wordCounts["_" + words[i]] = (wordCounts["_" + words[i]] || 0) + 1;
+// }
+
+// const highest = Object.values(wordCounts).sort(function (a, b) {
+//   return b - a;
+// });
+
+// for(let i = 0; i < str.length; i++){
+
+// }
