6d0fdbe4f114fd7118bcec936fc53230dcf699c8
Claude Ishimwe <dpqb12haikuo@gmail.com>
2023-07-24 11:14:58 +0200

    Count Down Timer

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,38 @@
 console.log("Exercise 3 - Timer");
+const myTimer = document.querySelector("#myTimer");
+var count = 0;
+myTimer.addEventListener("click", function (e) {
+  const minus = document.querySelector("#minus");
+  const plus = document.querySelector("#plus");
+  const reset = document.querySelector("#reset");
+  const start = document.querySelector("#start");
+  const stop = document.querySelector("#stop");
+  const timerField = document.querySelector("#timerField");
+  var intervalId;
+
+  if (e.target == plus) {
+    count = count + 1;
+    timerField.innerHTML = count;
+  } else if (e.target == minus) {
+    count -= 1;
+    timerField.innerHTML = count;
+  } else if (e.target == reset) {
+    count = 0;
+    timerField.innerHTML = count;
+  }
+});
+start.addEventListener("click", function (e) {
+  e.target.classList.add("hide");
+  document.querySelector("#stop").classList.remove("hide");
+
+  intervalId = setInterval(() => {
+    timerField.innerHTML = count;
+    count--;
+    if (count == -1) {
+      clearInterval(intervalId);
+      intervalId = null;
+      document.querySelector("#stop").classList.add("hide");
+      e.target.classList.remove("hide");
+    }
+  }, 1000);
+});
